844ec09a13f29de8b29ab2de2563710854739175
Jovan Bravo <jovanbravo112@gmail.com>
2022-02-11 15:16:25 +0100

    Create livewire components

---
+++ app/Http/Livewire/ResultComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ResultComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.result-component');
+    }
+}
---
+++ resources/views/livewire/home-component.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h2 class="text-4xl">First Component </h2>
+    
+</div>
